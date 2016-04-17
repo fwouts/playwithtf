091b5997f3feaec56f943634c32c94ddac24641f
Francois Wouts <francois.wouts@gmail.com>
2016-04-17 15:13:21 +1000

    Try out placeholders.

--- play.py
+++ play.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 
 state = tf.Variable(0, name="counter")
-one = tf.constant(1)
-new_value = tf.add(state, one)
+added = tf.placeholder(tf.int32)
+new_value = tf.add(state, added)
 update = tf.assign(state, new_value)
 init_op = tf.initialize_all_variables()
 
+feed = {
+    added: 3,
+}
 with tf.Session() as sess:
-        sess.run(init_op)
-        print(sess.run(state))
+        sess.run(init_op, feed)
+        print(sess.run(state, feed))
         for _ in range(3):
-                sess.run(update)
-                print(sess.run(state))
+                sess.run(update, feed)
+                print(sess.run(state, feed))
